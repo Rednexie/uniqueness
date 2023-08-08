7c905b1a2859435b240050275f3067eb3d6058d9
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-08-08 21:41:51 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+import { rednexie, ipify, ipapi } from "./api.json";
+
+import { discord } from "./discord.js";
+
+import { guilded } from "./guilded.js";
+
+const api = rednexie; // the default ip api that is used.
